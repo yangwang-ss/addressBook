5441f6d6dcd9a5774861335cff5dd5764168920a
jack <guanqiao@guanqiaodeMacBook-Pro.local>
2019-04-13 23:43:25 +0800

    second

--- src/services/user.js
+++ src/services/user.js
@@ -1,8 +1,6 @@
 import request from '@/utils/request';
 
-export async function query() {
-  return request('/api/users');
-}
+
 
 export async function queryCurrent() {
   return request('/api/currentUser');
